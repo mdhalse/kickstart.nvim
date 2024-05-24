b899b72d9428cf91915febb31cc5ecfea738dda5
Matthew Halse <matthew.halse@hey.com>
2024-05-23 22:34:16 -0500

    Enable gleam lsp

--- init.lua
+++ init.lua
@@ -555,6 +555,8 @@ require('lazy').setup({
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+      require('lspconfig').gleam.setup {}
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
